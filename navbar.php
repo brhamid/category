54f8055e2dc350fc295bc31df43b877a7e9617e8
brhamid <119353920+brhamid@users.noreply.github.com>
2023-01-10 15:00:02 +0400

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        #add{
+            align-items: center;
+            font-size:18px;
+            
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
+
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="posts.php">Əsas Səhifə</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">IT</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#"></a></li>
+      <li><a class="dropdown-item" href="#">PHP</a></li>
+      <li><a class="dropdown-item" href="#">JavaScript</a></li>
+      
+      <li><a class="dropdown-item" href="#">Java</a></li>
+    </ul>
+  </li>
+
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">İdman</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">Futbol</a></li>
+      <li><a class="dropdown-item" href="#">Basketbol</a></li>
+      <li><a class="dropdown-item" href="#">Tennis</a></li>
+      
+      
+    </ul>
+  </li>
+
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Elm</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">Kosmologiya</a></li>
+      <li><a class="dropdown-item" href="#">Kvant Fizikası</a></li>
+     
+    </ul>
+  </li>
+
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Felsefe</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">Analitik Felsefe</a></li>
+      <li><a class="dropdown-item" href="#">Qite Felsefesi</a></li>
+      
+    
+      
+    </ul>
+  </li>
+  
+  <li>
+</ul>
+<a class="btn btn-primary" id="add" href="add.php">Əlavə Et</a></li>
+
